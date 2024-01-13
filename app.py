fbc4304a6cd33d7dfe6625f62abdcac9de26686d
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-01-13 16:46:39 +0900

    change temperature

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def load_data():
     with st.spinner(text="Medicine-GPT is Loading"):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="text-embedding-ada-002", temperature=0.5, system_prompt=system_prompt))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-16k-0613", temperature=0.3, system_prompt=system_prompt))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
