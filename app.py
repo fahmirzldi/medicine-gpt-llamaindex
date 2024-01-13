13c8e0da75c39de1fa9bb16779ac23a35c89e0de
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-01-13 17:15:00 +0900

    try turbo instruct

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def load_data():
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         # service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-16k-0613", temperature=0.2, system_prompt=system_prompt))
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="davinci-002", temperature=0.2, system_prompt=system_prompt))
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.2, system_prompt=system_prompt))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
