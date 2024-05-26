02b9497d0113a18b823e6be12e0ca330d3bd1736
Fahmi Rizaldi <fahmi.rizaldi@gmail.com>
2024-05-26 17:47:25 +0900

    change import core

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
@@ -24,12 +24,15 @@ Example of available medicine: **doxycycline**, **corticotropin**, or **Tenormin
 """
 )
 
-if "messages" not in st.session_state.keys(): # Initialize the chat message history
+if "messages" not in st.session_state.keys():  # Initialize the chat message history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about your Medicine! Try: What do you know about doxycyline?"}
+        {
+            "role": "assistant",
+            "content": "Ask me a question about your Medicine! Try: What do you know about doxycyline?",
+        }
     ]
 
-system_prompt="""
+system_prompt = """
 You are an expert Pharmacist and your job is to answer basic question on medical drugs. Follow this rule for every question:
 - ONLY ANSWER BASED ON THE GIVEN CONTEXT INFORMATION / CSV DATASET. JUST STATE THE FACTS PRESENT THERE. DO NOT HALLUCINATE OR ADD ANY EXTRA INFORMATION OUTSIDE THE GIVEN CONTEXT / CSV DATASET.
 - Check if the drug name is present in the CSV dataset. If present, Give these information: what is medicine used for, common side effects in bullet points based on the information, and link source. If there is no information available, Tell user there is no information available regarding the medicine and ask to consult professional healthcare. 
@@ -37,24 +40,36 @@ You are an expert Pharmacist and your job is to answer basic question on medical
 - Answer in pretty and easy to read format. 
 - Cite the link when asked for sources"""
 
+
 @st.cache_resource(show_spinner=False)
 def load_data():
     with st.spinner(text="Medicine-GPT is Loading"):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-16k-0613", temperature=0.2, system_prompt=system_prompt))
+        service_context = ServiceContext.from_defaults(
+            llm=OpenAI(
+                model="gpt-3.5-turbo-16k-0613",
+                temperature=0.2,
+                system_prompt=system_prompt,
+            )
+        )
         # service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.5, system_prompt=system_prompt))
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
         return index
 
+
 index = load_data()
 
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, streaming=True)
+chat_engine = index.as_chat_engine(
+    chat_mode="condense_question", verbose=True, streaming=True
+)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+if prompt := st.chat_input(
+    "Your question"
+):  # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
+for message in st.session_state.messages:  # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
@@ -65,4 +80,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             response = chat_engine.chat(prompt)
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+            st.session_state.messages.append(message)  # Add response to message history
